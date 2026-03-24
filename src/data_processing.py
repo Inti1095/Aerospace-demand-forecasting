b5bf085258462c0949a0deffcda2e72d6b5ae207
Inti1095 <suyos1@gmail.com>
2026-03-23 22:08:05 -0400

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def load_data(path):
+    return pd.read_csv(path)
+
+def clean_data(df):
+    df = df.dropna()
+    return df
+
+def save_processed_data(df, path):
+    df.to_csv(path, index=False)
